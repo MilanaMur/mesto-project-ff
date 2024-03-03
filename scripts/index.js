b2a2d08428d49fbe38eb38715ffc5ea08792a17d
MilanaMur <milana.murashkina@yandex.ru>
2024-03-03 19:00:16 +0300

    Сделано ТЗ

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,31 @@
-// @todo: Темплейт карточки
+// Темплейт карточки
+const cardTemplate = document.querySelector('#card-template').content;
 
-// @todo: DOM узлы
+// DOM узлы
+const cardList = document.querySelector('.places__list');
 
-// @todo: Функция создания карточки
+// Функция создания карточки
+function createCard(item, deleteCard) {
+  const card = cardTemplate.querySelector('.card').cloneNode(true);
+  const cardImage = card.querySelector('.card__image');
+  const cardDeleteBtn = card.querySelector('.card__delete-button');
+  
+  cardImage.src = item.link;
+  cardImage.alt = item.name;
+  card.querySelector('.card__title').textContent = item.name;
 
-// @todo: Функция удаления карточки
+  cardDeleteBtn.addEventListener('click', () => deleteCard(card));
 
-// @todo: Вывести карточки на страницу
+  return card;
+}
+
+// Функция удаления карточки
+function deleteCard(cardForDelete) {
+  cardForDelete.remove();
+}
+
+// Вывод карточек на страницу
+initialCards.forEach(function (card) {
+  const newCard = createCard(card, deleteCard);
+  cardList.append(newCard);
+});
